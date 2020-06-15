296a508404daa19381e1bcc91c5145d2e3b90266
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-06-15 09:30:54 +0300

    add css and edit routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,13 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
+    
+    app.get { req -> EventLoopFuture<View> in
+        struct Context: Encodable {
+            let title: String
+            let body: String
+        }
+        let context = Context(title: "BlogModuleServer - Leaf", body: "Hello Leaf!")
+        return req.view.render("index", context)
     }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
-
-    try app.register(collection: TodoController())
 }
